d4f7e24445dbf28748676cf82afcc9e3d469b904
Maria Carolina <demycar24@gmail.com>
2024-01-17 00:29:19 -0300

    post

---
+++ form_post_/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="cadastro.php" method="post">
+        <div>
+            <input type="text" name="marca" placeholder="Marca do carro">
+        </div>
+        <div>
+            <input type="text" name="preco" placeholder="Preço do carro">
+        </div>
+        <div>
+            <input type="checkbox" name="opcionais[]" value="Teto solar"> Teto solar
+        </div>
+        <div>
+            <input type="checkbox" name="opcionais[]" value="Película">Película
+        </div>
+        <div>
+            <input type="checkbox" name="opcionais[]" value="Blindado">Blindado
+        </div>
+        <div>
+            <input type="submit" value="Enviar">
+        </div>
+    </form>
+</body>
+
+</html>
